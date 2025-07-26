de1c0974b90242b173377d443b2f6909520ab3af
SeungYeop Oh <sadrain9274@naver.com>
2025-07-26 13:10:07 +0900

    [Feat] player 이동 코드 작성 및 키맵핑 설정
    PlayerController - OnMove OnLook
    player Input controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : BaseController
 {
     private Camera camera;
+
+    protected override void Start()
+    {
+        base.Start();
+        camera = Camera.main;
+    }
+
+    void OnMove(InputValue inputValue)
+    {
+        movementDirection = inputValue.Get<Vector2>();
+        movementDirection = movementDirection.normalized;
+    }
+
+    void OnLook(InputValue inputValue)
+    {
+        Vector2 mousePosition = inputValue.Get<Vector2>();
+        Vector2 worldPos = camera.WorldToScreenPoint(mousePosition);
+        lookDirection = Vector2.zero;
+
+        if(lookDirection.magnitude < .9f)
+        {
+            lookDirection = Vector2.zero;
+        }
+        else
+        {
+            lookDirection = lookDirection.normalized;
+        }
+    }
 }
