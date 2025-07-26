8833bec6d1c59ea1bf5734a731bbf1566abb9b6e
SeungYeop Oh <sadrain9274@naver.com>
2025-07-26 11:44:57 +0900

    [Feat] 움직임에 필요한 필드 및 프로퍼티 작성
    움직인느 방향, 바라보는 방향

--- Assets/Scripts/BaseController.cs
+++ Assets/Scripts/BaseController.cs
@@ -6,6 +6,14 @@ public class BaseController : MonoBehaviour
 {
     protected Rigidbody2D _rigidbody;
 
+    [SerializeField] private SpriteRenderer playerRenderer;
+
+    protected Vector2 movementDirection = Vector2.zero;
+    public Vector2 MovementDirection { get { return movementDirection; } }
+
+    protected Vector2 lookDirection = Vector2.zero;
+    public Vector2 LookDirection { get { return lookDirection; } }
+
     private void Movement(Vector2 direction)
     {
         direction = direction * 5;
