65d80e3b000c6ed33198247c96938f3d759f054c
SeungYeop Oh <sadrain9274@naver.com>
2025-07-28 07:30:10 +0900

    [Fix] 미니게임 트리거 이름 변경

---
+++ Assets/SpartanMetaverse/Scripts/MiniGameTrigger.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MiniGameTrigger : MonoBehaviour
+{
+    [SerializeField] private string sceneName; //불러오고자 하는 씬 이름
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+
+            if (UIManager.instance != null)
+            {
+                UIManager.instance.ShowPanelUI(sceneName);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+
+            if (UIManager.instance != null)
+            {
+                UIManager.instance.HidePanelUI();
+            }
+        }
+    }
+}
