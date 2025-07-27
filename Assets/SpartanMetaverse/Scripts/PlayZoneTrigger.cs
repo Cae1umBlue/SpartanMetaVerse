edb56e251a8e97f546e5cc66ac2f78cd9e5604f2
SeungYeop Oh <sadrain9274@naver.com>
2025-07-27 13:28:25 +0900

    [Chore] 주석 삭제

--- Assets/SpartanMetaverse/Scripts/PlayZoneTrigger.cs
+++ Assets/SpartanMetaverse/Scripts/PlayZoneTrigger.cs
@@ -7,14 +7,12 @@ public class PlayZoneTrigger : MonoBehaviour
 {
     [SerializeField] private Scenes sceneName;
     public Scenes TargetScene => sceneName;
-    //public bool IsPlayerInside() => isPlayerInside;
-    //private bool isPlayerInside = false; // 플레이어가 플레이존에 들어왔는지 유무
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-/*            isPlayerInside = true*/;
+
             if (UIManager.instance != null)
             {
                 UIManager.instance.ShowPanelUI(this);
@@ -26,7 +24,7 @@ public class PlayZoneTrigger : MonoBehaviour
     {
         if (collision.CompareTag("Player"))
         {
-            //isPlayerInside = false;
+
             if (UIManager.instance != null)
             {
                 UIManager.instance.HidePanelUI();
